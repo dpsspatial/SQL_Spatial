3aac3a6f4bca4a4097987b14fecf93c3d812ad8d
dpsspatial <matthew_baker@dpsk12.org>
2016-12-14 09:16:13 -0700

    updates

---
+++ Create_Atlas_BoundingBoxes.sql
@@ -0,0 +1,20 @@
+﻿ --drop table public.temp_oct_bbox;
+ --create table public.temp_oct_bbox as (
+select 
+oct.schnum
+, sch.school_name
+, sch.elem
+, sch.mid
+, sch.high
+, ST_Envelope(ST_Collect(oct.geom)) as geom
+
+from dpsdata."OctoberCount_Archive" as oct
+, public."CCD_Neighborhoods" as h
+
+join dpsdata."Schools_Current" as sch on oct.schnum = cast(sch.schnum as varchar)
+
+where ST_Intersects(oct.geom, h.geom)
+and oct.year = '2016'
+group by oct.schnum,sch.school_name,sch.elem, sch.mid, sch.high
+
+)
